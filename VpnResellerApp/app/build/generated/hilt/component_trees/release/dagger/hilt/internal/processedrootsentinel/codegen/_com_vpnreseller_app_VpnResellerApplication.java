7f41bc8dd28ec92711e602d3b5776a5f9b583862
gg_pot <132027512+pofarshad@users.noreply.github.com>
2025-05-25 21:35:42 +0000

    Committing changes

--- VpnResellerApp/app/build/generated/hilt/component_trees/release/dagger/hilt/internal/processedrootsentinel/codegen/_com_vpnreseller_app_VpnResellerApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.vpnreseller.app.VpnResellerApplication"
-)
-public final class _com_vpnreseller_app_VpnResellerApplication {
-}
