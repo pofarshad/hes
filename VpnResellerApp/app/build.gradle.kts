e31cd6d0f13aac962e504279df7d36180dde311f
gg_pot <132027512+pofarshad@users.noreply.github.com>
2025-05-25 14:14:57 +0000

    Add com.google.android.material:material dependency to fix theme resource linking error

--- VpnResellerApp/app/build.gradle.kts
+++ VpnResellerApp/app/build.gradle.kts
@@ -75,6 +75,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
+    implementation("com.google.android.material:material:1.9.0")
+
     // ViewModel
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
