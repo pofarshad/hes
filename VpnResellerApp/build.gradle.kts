07c543a4f3ff333b6b8abf2ae12212478ed878d2
Mehdi Pourtaghi <me.pourtaghi@gmail.com>
2025-05-26 15:35:14 +0330

    fix: access catalog versions with .toString() in root build.gradle.kts

--- VpnResellerApp/build.gradle.kts
+++ VpnResellerApp/build.gradle.kts
@@ -1,11 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    id(libs.plugins.android.application.get().pluginId) version libs.versions.agp.get() apply false
-    id(libs.plugins.android.library.get().pluginId) version libs.versions.agp.get() apply false
-    id(libs.plugins.kotlin.android.get().pluginId) version libs.versions.kotlin.get() apply false
-    id(libs.plugins.hilt.get().pluginId) version libs.versions.hilt.get() apply false
-    // google-services plugin is applied in app/build.gradle.kts, so it's not needed here with apply false
+    id(libs.plugins.android.application.get().pluginId) version libs.versions.agp.toString() apply false
+    id(libs.plugins.android.library.get().pluginId) version libs.versions.agp.toString() apply false
+    id(libs.plugins.kotlin.android.get().pluginId) version libs.versions.kotlin.toString() apply false
+    id(libs.plugins.hilt.get().pluginId) version libs.versions.hilt.toString() apply false
 }
 
 tasks.register("clean", Delete::class) {
