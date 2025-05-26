f3ecc42d93f483f18a09cabcedb614e5bc981a50
Mehdi Pourtaghi <me.pourtaghi@gmail.com>
2025-05-26 15:20:54 +0330

    fix: move repository declarations to settings.gradle.kts

--- VpnResellerApp/build.gradle.kts
+++ VpnResellerApp/build.gradle.kts
@@ -1,9 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
     dependencies {
         classpath(libs.android.gradle)
         classpath(libs.kotlin.gradle)
